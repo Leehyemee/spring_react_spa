82e1884d076b663f4e334ffd1bd4bb2efc5e1af0
hyemee <svsvdvdv@naver.com>
2025-03-27 09:40:08 +0900

    list 엔드포인트 변경 - @PathVariable

--- SemiProjectV2/src/main/java/com/example/svsvdvdv/semiprojectv2/controller/BoardController.java
+++ SemiProjectV2/src/main/java/com/example/svsvdvdv/semiprojectv2/controller/BoardController.java
@@ -37,8 +37,11 @@ public class BoardController {
         return response;
     }
 
-    @GetMapping("/list")
-    public ResponseEntity<?> list(@RequestParam(defaultValue = "1") int cpg) {
+    // list 엔드포인트 변경
+    // http://localhost:8080/api/board/list?cpg=4 아래로 변경(/list -> /list/{cpg} , RequestParam -> PathVariable)
+    // http://localhost:8080/api/board/list/4
+    @GetMapping("/list/{cpg}")
+    public ResponseEntity<?> list(@PathVariable int cpg) {
         BoardListDTO boardListDTO = boardService.readBoard(cpg);
 
         return new ResponseEntity<>(boardListDTO, HttpStatus.OK);
