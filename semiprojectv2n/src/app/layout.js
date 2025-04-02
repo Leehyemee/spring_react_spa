88d02f17073edc7527a5a510185a1cb465d8b5f1
hyemee <svsvdvdv@naver.com>
2025-04-02 11:39:52 +0900

    기본 페이지 수정 - semiprojectv2 home

--- semiprojectv2n/src/app/layout.js
+++ semiprojectv2n/src/app/layout.js
@@ -1,5 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./layout/Header";
+import Nav from "./layout/Nav";
+import Footer from "./layout/Footer";
 
 
 
@@ -17,11 +19,14 @@ export default function RootLayout({ children }) {
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet"/>
       </head>
       <body>
-          <div className="container">
-            <div className="content-wrapper mx-auto">
-              {children}
-            </div>
-          </div>
+      <div className="container">
+        <div className="content-wrapper mx-auto">
+            <Header />
+            <Nav />
+             {children}
+            <Footer />
+        </div>
+      </div>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></script>
       </body>
       </html>
