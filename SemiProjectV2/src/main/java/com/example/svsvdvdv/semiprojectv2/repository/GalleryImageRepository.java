b401beff8c58060fa6cc7354f1f351675db30741
hyemee <svsvdvdv@naver.com>
2025-04-04 10:25:50 +0900

    갤러리 구현을 위한 JPA 관련 객체 생성

---
+++ SemiProjectV2/src/main/java/com/example/svsvdvdv/semiprojectv2/repository/GalleryImageRepository.java
@@ -0,0 +1,8 @@
+package com.example.svsvdvdv.semiprojectv2.repository;
+
+import com.example.svsvdvdv.semiprojectv2.domain.GalleryImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GalleryImageRepository extends JpaRepository<GalleryImage, Long> {
+
+}
---
+++ SemiProjectV2/src/main/java/com/example/svsvdvdv/semiprojectv2/repository/GalleryRepository.java
@@ -0,0 +1,9 @@
+package com.example.svsvdvdv.semiprojectv2.repository;
+
+//
+import com.example.svsvdvdv.semiprojectv2.domain.Gallery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GalleryRepository extends JpaRepository<Gallery, Long> {
+
+}
