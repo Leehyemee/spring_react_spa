18110de36cac866711c1fa28cc1ba997e337f2e5
hyemee <svsvdvdv@naver.com>
2025-04-07 09:26:12 +0900

    자료실 관련 repository 구현

---
+++ SemiProjectV2/src/main/java/com/example/svsvdvdv/semiprojectv2/repository/PdsRepository.java
@@ -0,0 +1,8 @@
+package com.example.svsvdvdv.semiprojectv2.repository;
+
+import com.example.svsvdvdv.semiprojectv2.domain.Pds;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PdsRepository extends JpaRepository<Pds, Long> {
+
+}
